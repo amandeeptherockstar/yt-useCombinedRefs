495866e72392859f19110fecd76ccf060271e79b
Amandeep Kochhar <amandeeptherockstar@gmail.com>
2025-03-30 14:32:02 +0530

    Starter Code

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,23 +1,7 @@
-import { Controller, Control } from "react-hook-form";
 import InputField from "./InputField";
 
-interface SearchFormValues {
-  search: string;
-}
-
-interface SearchInputProps {
-  control: Control<SearchFormValues>;
-  name: keyof SearchFormValues;
-}
-
-function SearchInput({ control, name }: SearchInputProps) {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field }) => <InputField field={field} />}
-    />
-  );
+function SearchInput() {
+  return <InputField />;
 }
 
 export default SearchInput;
--- src/pages/ProductSearchPage.tsx
+++ src/pages/ProductSearchPage.tsx
@@ -1,19 +1,6 @@
-import { useForm } from "react-hook-form";
 import SearchInput from "../components/SearchInput";
 
-interface SearchFormValues {
-  search: string;
-}
-
 function ProductSearchPage() {
-  const { control, handleSubmit } = useForm<SearchFormValues>({
-    defaultValues: { search: "" },
-  });
-
-  const onSubmit = (data: SearchFormValues) => {
-    alert(`You searched: ${data.search}`);
-  };
-
   return (
     <div className="min-h-screen bg-gray-50">
       <header className="bg-white shadow">
@@ -22,11 +9,8 @@ function ProductSearchPage() {
         </div>
       </header>
       <main className="mx-auto max-w-7xl px-4 py-6">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-x-4"
-        >
-          <SearchInput name="search" control={control} />
+        <form className="flex flex-col space-x-4">
+          <SearchInput />
           <div className="mt-4">
             <button
               type="submit"
